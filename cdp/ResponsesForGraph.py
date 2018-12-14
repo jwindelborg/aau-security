a6506553fac366d13c86dfffdb1024ec411cd2ed
Mathias Leding <mathias@leding.dk>
2018-12-14 14:33:57 +0100

    broken. skal fikses :)

--- cdp/ResponsesForGraph.py
+++ cdp/ResponsesForGraph.py
@@ -3,23 +3,22 @@
 import pychrome
 import time
 import mysql.connector
-import pychrome
 import base64
+import PyChromeDevTools
+import json
+
 
 def do_page(page):
     starttime = time.time()
     endtime = starttime+(60*5)
 
     chrome = PyChromeDevTools.ChromeInterface()
-    chrome.Memory.startSampling()
+    # chrome.Memory.startSampling()
     chrome.Network.enable()
     chrome.Page.enable()
 
     chrome.Page.navigate(url="http://" + page)
     chrome.wait_event("Page.frameStoppedLoading", timeout=60)
-
-    data = chrome.Page.captureScreenshot()
-    responses = []
 
     db = mysql.connector.connect(
                     host="ssh.windelborg.info", 
@@ -28,27 +27,37 @@ def do_page(page):
                     database="aau")
     cursor = db.cursor()
 
-    while 1==1:
-        if(time.time() >= endtime):
-            break
-        response = chrome.wait_message(10)
-        letime = time.time()-starttime
-        tupl = (letime,response)
+    responses = []
+    data = chrome.Page.captureScreenshot()
+
+    while true:
+        if time.time() > endtime:
+            print("hej")
+        response = chrome.wait_message(1)
+        if response is None:
+            continue
+        curtime = time.time()-starttime
+      #  responsedump = json.dumps(response)
+        tupl = (curtime, "hej")
         responses.append(tupl)
+    if not responses:
+        return
+#    sql = "INSERT INTO Responses(response_time, messages) VALUES (%s, %s)"
+#    cursor.executemany(sql, responses)
+    #db.commit()
+    #db.close()
 
-    sql = "INSERT INTO Responses (response_time, message) VALUES (%s, %s)"
-    cursor.executemany(sql,responses)
-    db.commit()
-    db.close()
+    # with open(page, "w") as f:
+    #    for item in responses:
+    #        f.write("%s\n" % str(item))
 
-    with open(page, "w") as f:
-        for item in responses:
-            f.write("%s\n" % str(item))
 
 with open("top100") as file:
     for line in file:
-        try:
-            do_page(line.strip())
-        except:
-            continue
+        do_page(line.strip())
+
+        #try:
+        #    do_page(line.strip())
+        #except:
+        #    continue
 
