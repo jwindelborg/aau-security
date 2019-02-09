71c25e6f6008840d6223555551009aca4499193c
Jesper Nielsen <jesper@windelborg.dk>
2019-02-09 14:10:55 +0100

    Update cdp protocol, now ready for first test

--- go-cdp-yourself/main.go
+++ go-cdp-yourself/main.go
@@ -1,9 +1,5 @@
 package main
 
-// Before use
-// docker run -d -p 9222:9222 --rm --name headless-shell chromedp/headless-shell
-//
-
 import (
 	"context"
 	"database/sql"
@@ -20,6 +16,7 @@ import (
 	"io/ioutil"
 	_ "io/ioutil"
 	"log"
+	"net/http"
 	"regexp"
 	"strings"
 	"time"
@@ -31,48 +28,39 @@ type Domain struct {
 }
 
 func main() {
-
-	//var domains = loadDomains(`zonefile.txt`)
-
 	var err error
+
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	db, err := sql.Open("mysql", "aau:2387AXumK52aeaSA@tcp(85.191.223.61:3306)/aau")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// create context
-	ctxt, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	domains := loadDomainsDB(ctxt, *db)
 
-	var domains = loadDomainsDB(ctxt, *db)
-	var numDomains = len(domains)
-
-	// create chrome instance
 	c, err := chromedp.New(ctxt,
 		chromedp.WithRunnerOptions(
-			//runner.ProxyServer("http://127.0.0.1:8080"), // enable for mitmproxy or Burp
-			runner.Flag("headless", true),
+			runner.ProxyServer("http://127.0.0.1:8080"), // enable for mitmproxy or Burp
+			runner.Flag("headless", true),    // enable for server, disable for local debug
 			runner.Flag("no-sandbox", true),
 		),
-		//chromedp.WithLog(log.Printf),
+		//chromedp.WithLog(log.Printf), // Verbose output
 	)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < numDomains - 1; i++ {
+	for i := 0; i < len(domains) - 1; i++ {
 		doDomain(ctxt, c, *db, domains[i])
 	}
 
-	// shutdown chrome
 	err = c.Shutdown(ctxt)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// wait for chrome to finish
 	err = c.Wait()
 	if err != nil {
 		log.Fatal(err)
@@ -85,8 +73,10 @@ func doDomain(ctxt context.Context,c *chromedp.CDP, db sql.DB, domain Domain) dw
 	var title string
 	var resJSbase64 string
 
+	// TODO: Find a way to access DevTools network tab
 	tasks = append(tasks, chromedp.Tasks{
-		security.SetIgnoreCertificateErrors(true),
+		security.SetIgnoreCertificateErrors(true), // if intercept with burp or mitmproxy certificate is not signed
+		// TODO: Find a way to access profiler data, it gives specific knowledge about run JavaScript
 		profiler.Enable(),
 		profiler.Start(),
 		profiler.StartPreciseCoverage(),
@@ -94,8 +84,16 @@ func doDomain(ctxt context.Context,c *chromedp.CDP, db sql.DB, domain Domain) dw
 		chromedp.Sleep(10*time.Second),
 		chromedp.Stop(),
 		chromedp.Title(&title),
-		chromedp.EvaluateAsDevTools("var scri = []; for (var index = 0; index < document.getElementsByTagName('script').length; index++) { scri[index] = (document.getElementsByTagName('script')[index].outerHTML.toString()); }; btoa(scri.join('::,,//'));", &resJSbase64),
+		chromedp.EvaluateAsDevTools("var scri = []; for (var index = 0; index < document.getElementsByTagName('script').length; index++) { scri[index] = (document.getElementsByTagName('script')[index].outerHTML.toString()); }; btoa(unescape(encodeURIComponent(scri.join('::,,//'))));", &resJSbase64),
+									// var script = []
+									// var scriptObj = document.getElementByTagName('script')
+									// for (var index = 0; index < scriptObj; index++) {
+									//   script[i] = (script.Obj.outerHTML.ToString());
+									// }
+									// btoa(unescape(encodeURIComponent(script.join('::,,//')))));
 	})
+	// Consider setting a flag between sites for mitmproxy
+	// navigate(Domain.id + ".arpa") or similar
 
 	err = c.Run(ctxt, chromedp.Tasks{tasks})
 	if err != nil {
@@ -115,7 +113,7 @@ func doDomain(ctxt context.Context,c *chromedp.CDP, db sql.DB, domain Domain) dw
 	scripts := strings.Split(string(resJSbase64Decoded),"::,,//")
 
 	for _, element := range scripts {
-		var findScriptRegex = regexp.MustCompile(`<script\s+[^>]*?src=(("|')([^"']+))`)
+		findScriptRegex := regexp.MustCompile(`<script\s+[^>]*?src=(("|')([^"']+))`)
 
 		if !findScriptRegex.MatchString(element) {
 			shabytes := sha3.Sum256([]byte(element))
@@ -124,25 +122,24 @@ func doDomain(ctxt context.Context,c *chromedp.CDP, db sql.DB, domain Domain) dw
 			_, err = db.Exec(sqlJavaScriptInsert, element, sha)
 			sqlJavaScriptRelationInsert := `INSERT INTO javascriptdomain (javascript_checksum, domain_id) VALUES (?, ?);`
 			_, err = db.Exec(sqlJavaScriptRelationInsert, sha, domain.id)
-		} //else {
+		} else {
+			scriptURL := findScriptRegex.FindStringSubmatch(element)[3]
+			scriptURL = prepareScriptURL(domain.domain, scriptURL)
 
-			// Handled by mitmproxy
-			// TODO: Handle this by mitmproxy
+			response, _ := http.Get(scriptURL)
+			if response.StatusCode >= 200 && response.StatusCode < 400 {
+				body, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					log.Fatal(err)
+				} else {
+					defer response.Body.Close()
+				}
+				element = string(body)
+			} else {
+				element = string(response.StatusCode)
+			}
 
-			//var scriptURL = findScriptRegex.FindStringSubmatch(element)[3]
-			//
-			//if scriptURL != "" {
-			//	response, err := http.Get(scriptURL)
-			//	body, err := ioutil.ReadAll(response.Body)
-			//	if err != nil {
-			//		//log.Fatal(err)
-			//		log.Printf("not a valid url")
-			//	} else {
-			//		defer response.Body.Close()
-			//	}
-			//	element = string(body)
-			//}
-		//}
+		}
 	}
 
 	return dwarf.VoidType{}
@@ -155,7 +152,7 @@ func loadDomainsDB(ctxt context.Context, db sql.DB) []Domain {
 	}
 	defer rows.Close()
 
-	var domains = []Domain{}
+	domains := []Domain{}
 
 	for rows.Next() {
 		var (
@@ -167,12 +164,27 @@ func loadDomainsDB(ctxt context.Context, db sql.DB) []Domain {
 		}
 		tmpDom := Domain{
 			id: id,
-			domain: domain,
+			domain: strings.TrimSpace(domain),
 		}
 		domains = append(domains, tmpDom)
 	}
 
 	return domains
+}
+
+func prepareScriptURL(domain string, url string) string {
+	url = strings.TrimSpace(url)
+	if strings.Contains(url, "\n") {
+		url = strings.Trim(url, "\n")
+	}
+
+	if strings.HasPrefix(url, "http://") || strings.HasPrefix(url,"https://") {
+		return url
+	} else if strings.HasPrefix(url,"//") {
+		return "http://" + domain + url
+	} else {
+		return "http://" + domain + url
+	}
 }
 
 func loadDomains(filename string) []string  {
