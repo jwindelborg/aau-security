f3249ab1d9e3f295cee0cbfeb981b6f1a6f611a2
Jesper Nielsen <jesper@windelborg.dk>
2019-06-03 00:07:31 +0200

    Update database_schema and delete garbage file

--- database_schema.sql
+++ database_schema.sql
@@ -11,7 +11,8 @@ create table domains
 (
 	domain_id int auto_increment primary key,
 	domain varchar(128) not null,
-	created_at timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP
+	created_at timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+	constraint domains_all_pk unique (domain)
 );
 
 create table cdp_visit_history
@@ -21,9 +22,7 @@ create table cdp_visit_history
 	worker varchar(253) null,
 	created_at datetime null,
 	scan_label varchar(250) null,
-	constraint domain_id_foregin_key
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade
+	constraint domain_id_foregin_key foreign key (domain_id) references domains (domain_id) on update cascade
 );
 
 create table cookies
@@ -38,13 +37,10 @@ create table cookies
 	created_at datetime null,
 	scan_label varchar(255) not null,
 	primary key (domain_id, cookie_name, cookie_domain, scan_label),
-	constraint cookies_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint cookies_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
-create index domain_id
-	on cookies (domain_id);
+create index domain_id on cookies (domain_id);
 
 create table domain_has_cms_vulnerabilities
 (
@@ -52,12 +48,9 @@ create table domain_has_cms_vulnerabilities
 	vulnerability_id varchar(250) not null,
 	created_at datetime null,
 	primary key (domain_id, vulnerability_id),
-	constraint domain_has_vulnerability_vulnerability
-		foreign key (vulnerability_id) references cms_vulnerabilities (vulnerability_id)
-			on update cascade on delete cascade,
-	constraint domain_id_foregin
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint domain_has_cms_vuln_domain_id
+		foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade,
+	constraint domain_has_cms_vuln_vuln_id foreign key (vulnerability_id) references cms_vulnerabilities (vulnerability_id) on update cascade on delete cascade
 );
 
 create table hsts_policies
@@ -65,22 +58,18 @@ create table hsts_policies
 	domain_id int not null primary key,
 	policy varchar(250) null,
 	created_at datetime null,
-	constraint htst_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint htst_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table http_headers
 (
 	domain_id int not null,
-	request_url varchar(1000) not null,
+	request_url varchar(900) not null,
 	scan_label varchar(50) not null,
 	created_at datetime null,
 	header text not null,
 	primary key (domain_id, request_url, scan_label),
-	constraint http_header_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint http_header_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table identified_cms
@@ -88,14 +77,12 @@ create table identified_cms
 	domain_id int not null primary key,
 	cms_system varchar(250) null,
 	created_at datetime null,
-	constraint identified_cms_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint identified_cms_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table javascript_vulnerabilities
 (
-	vulnerability_id varchar(512) not null primary key,
+	vulnerability_id varchar(250) not null primary key,
 	vulnerability_description text not null,
 	severity int not null,
 	created_at datetime null
@@ -103,7 +90,7 @@ create table javascript_vulnerabilities
 
 create table javascripts
 (
-	javascript_hash varchar(1000) not null primary key,
+	javascript_hash varchar(250) not null primary key,
 	script longtext not null,
 	created_at datetime null
 );
@@ -111,35 +98,29 @@ create table javascripts
 create table domain_has_javascripts
 (
 	domain_id int not null,
-	javascript_hash varchar(800) not null,
+	javascript_hash varchar(250) not null,
 	is_external int not null,
 	url varchar(1000) null,
 	scan_label varchar(100) not null,
 	created_at datetime null,
 	primary key (domain_id, javascript_hash, scan_label),
-	constraint domain_has_javascript_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade,
-	constraint domain_has_javascript_javascript
-		foreign key (javascript_hash) references javascripts (javascript_hash)
-			on update cascade on delete cascade
+	constraint domain_has_javascript_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade,
+	constraint domain_has_javascript_hash foreign key (javascript_hash) references javascripts (javascript_hash) on update cascade on delete cascade
 );
 
 create table javascript_analyzes
 (
 	analyze_id int auto_increment primary key,
-	javascript_hash varchar(1000) null,
+	javascript_hash varchar(250) null,
 	analytic_tool varchar(512) null,
 	worker varchar(253) null,
 	created_at datetime null,
-	constraint javascript_analyzes_hash
-		foreign key (javascript_hash) references javascripts (javascript_hash)
-			on update cascade on delete cascade
+	constraint javascript_analyzes_hash foreign key (javascript_hash) references javascripts (javascript_hash) on update cascade on delete cascade
 );
 
 create table libraries
 (
-	library_id varchar(60) not null primary key,
+	library_id varchar(250) not null primary key,
 	library_name varchar(60) not null,
 	library_version varchar(60) not null,
 	created_at datetime null
@@ -147,30 +128,22 @@ create table libraries
 
 create table javascript_is_library
 (
-	javascript_hash varchar(940) not null,
-	library_id varchar(60) not null,
+	javascript_hash varchar(250) not null,
+	library_id varchar(250) not null,
 	created_at datetime null,
 	primary key (javascript_hash, library_id),
-	constraint javascript_is_library_hash
-		foreign key (javascript_hash) references javascripts (javascript_hash)
-			on update cascade on delete cascade,
-	constraint javascript_is_library_library
-		foreign key (library_id) references libraries (library_id)
-			on update cascade on delete cascade
+	constraint javascript_is_library_javascript_hash foreign key (javascript_hash) references javascripts (javascript_hash) on update cascade on delete cascade,
+	constraint javascript_is_library_library foreign key (library_id) references libraries (library_id) on update cascade on delete cascade
 );
 
 create table library_has_vulnerabilities
 (
-	library_id varchar(60) not null,
-	vulnerability_id varchar(60) not null,
+	library_id varchar(250) not null,
+	vulnerability_id varchar(250) not null,
 	created_at datetime null,
 	primary key (library_id, vulnerability_id),
-	constraint library_has_vulnerability_libid
-		foreign key (library_id) references libraries (library_id)
-			on update cascade on delete cascade,
-	constraint library_has_vulnerability_vuln_id
-		foreign key (vulnerability_id) references javascript_vulnerabilities (vulnerability_id)
-			on update cascade on delete cascade
+	constraint library_has_vuln_vuln_id foreign key (vulnerability_id) references javascript_vulnerabilities (vulnerability_id) on update cascade on delete cascade,
+	constraint library_has_vulnerability_libid foreign key (library_id) references libraries (library_id) on update cascade on delete cascade
 );
 
 create table locked_domains
@@ -180,9 +153,7 @@ create table locked_domains
 	created_at datetime null,
 	scan_label varchar(100) not null,
 	primary key (domain_id, scan_label),
-	constraint locked_domains_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint locked_domains_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table locked_ssl_scan
@@ -191,9 +162,7 @@ create table locked_ssl_scan
 	worker varchar(250) not null,
 	created_at datetime null,
 	primary key (domain_id, worker),
-	constraint locked_ssl_scan_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint locked_ssl_scan_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table powered_by
@@ -201,9 +170,7 @@ create table powered_by
 	domain_id int not null primary key,
 	x_powered_by varchar(250) null,
 	created_at datetime null,
-	constraint powered_by_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint powered_by_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table privacy_badger_actions
@@ -214,22 +181,18 @@ create table privacy_badger_actions
 	created_at datetime not null,
 	scan_label varchar(100) not null,
 	primary key (domain_id, is_red, concerning, scan_label),
-	constraint privacy_badger_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint privacy_badger_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table server_software
 (
-	software_hash varchar(500) not null,
+	software_hash varchar(250) not null,
 	domain_id int not null,
 	software varchar(150) null,
 	version varchar(150) null,
 	created_at datetime null,
 	primary key (software_hash, domain_id),
-	constraint server_software_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint server_software_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table server_vulnerabilities
@@ -245,12 +208,8 @@ create table server_software_has_server_vulnerabilities
 	cve varchar(250) not null,
 	software_hash varchar(250) not null,
 	primary key (cve, software_hash),
-	constraint server_software_has_server_vuln_software_hash
-		foreign key (software_hash) references server_software (software_hash)
-			on update cascade on delete cascade,
-	constraint server_software_hash_vuln_vuln_id
-		foreign key (cve) references server_vulnerabilities (cve)
-			on update cascade on delete cascade
+	constraint server_software_has_server_vuln_software_id foreign key (software_hash) references server_software (software_hash) on update cascade on delete cascade,
+	constraint server_software_has_server_vuln_vuln_id foreign key (cve) references server_vulnerabilities (cve) on update cascade on delete cascade
 );
 
 create table ssl_properties
@@ -275,9 +234,7 @@ create table ssl_properties
 	has_lucky13 tinyint(1) not null,
 	has_rc4 tinyint(1) not null,
 	created_at datetime null,
-	constraint ssl_properties_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint ssl_properties_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
 
 create table ssl_scan_history
@@ -286,7 +243,7 @@ create table ssl_scan_history
 	worker varchar(250) null,
 	domain_id int null,
 	created_at datetime null,
-	constraint ssl_scan_history_domain_id
-		foreign key (domain_id) references domains (domain_id)
-			on update cascade on delete cascade
+	constraint ssl_scan_history_domain_id foreign key (domain_id) references domains (domain_id) on update cascade on delete cascade
 );
+
+
